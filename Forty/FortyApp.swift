a3aa9ab5def923a39603bc547d68e4c1e907ecc8
Jon Allured <jon.allured@gmail.com>
2021-11-21 14:55:52 -0600

    Initial Commit

---
+++ Forty/FortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FortyApp.swift
+//  Forty
+//
+//  Created by Jonathan Allured on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct FortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
